d77fc480f27043c0f733796e05781393f32cf402
Sophie Åkesson <sophie.akesson@gmail.com>
2021-02-08 17:40:56 +0100

    Add middleware, style fixes and refactoring

--- routes/index.js
+++ routes/index.js
@@ -1,57 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const Task = require("../model/Task");
+const deleteMiddleware = require("./middleware/delete");
+const loadTasks = require("./middleware/loadTasks");
 
-router.get("/", async (req, res) => {
-  const page = +req.query.page || 1;
-  const sort = req.query.sort || "asc";
-  const limit = 3;
-
-  try {
-    const tasks = await Task.find()
-      .limit(limit * 1)
-      .skip((page - 1) * limit)
-      .sort({ dueDate: sort });
-
-    const totalDocuments = await Task.countDocuments();
-
+router.get("/", loadTasks, (req, res) => {
     res.render("index.ejs", {
-      tasks,
-      totalPages: Math.ceil(totalDocuments / limit),
-      currentPage: page,
-      currentSort: sort,
-      totalDocuments,
+      tasks: req.headers.tasks,
+      totalPages: req.headers.totalPages,
+      currentPage: req.headers.currentPage,
+      currentSort: req.headers.currentSort,
+      totalDocuments: req.headers.totalDocuments,
     });
-  } catch (err) {
-    res.render("error.ejs", { err });
-  }
 });
 
-router.get("/edit", async (req, res) => {
-  const page = +req.query.page || 1;
-  const sort = req.query.sort || "asc";
-  const limit = 3;
-
-  try {
-    const tasks = await Task.find()
-      .limit(limit * 1)
-      .skip((page - 1) * limit)
-      .sort({ dueDate: sort });
-
-    const totalDocuments = await Task.countDocuments();
-    const taskId = req.query.id;
-
+router.get("/edit", loadTasks, (req, res) => {
     res.render("edit.ejs", {
-      tasks,
-      taskId,
-      totalPages: Math.ceil(totalDocuments / limit),
-      currentPage: page,
-      currentSort: sort,
-      totalDocuments,
+      tasks: req.headers.tasks,
+      taskId: req.headers.taskId,
+      totalPages: req.headers.totalPages,
+      currentPage: req.headers.currentPage,
+      currentSort: req.headers.currentSort,
+      totalDocuments: req.headers.totalDocuments,
     });
-  } catch (err) {
-    res.render("error.ejs", { err });
-  }
 });
 
 router.post("/edit", async (req, res) => {
@@ -59,6 +30,9 @@ router.post("/edit", async (req, res) => {
   const sort = req.query.sort || "asc";
 
   try {
+    if (req.body.editName === "") {
+      throw "Please pick a name for your task.";
+    }
     const changedTask = await Task.updateOne(
       { _id: req.query.id },
       { name: req.body.editName, dueDate: req.body.editDate }
@@ -85,49 +59,12 @@ router.post("/new", async (req, res) => {
     const savedTask = await newTask.save();
     res.redirect(301, "/?page=" + page + "&sort=" + sort);
   } catch (err) {
-    const tasks = await Task.find()
-      .limit(limit * 1)
-      .skip((page - 1) * limit)
-      .sort({ dueDate: sort });
-
-    const totalDocuments = await Task.countDocuments();
-    res.render("index.ejs", {
-      tasks,
-      message: err,
-      totalPages: Math.ceil(totalDocuments / limit),
-      currentPage: page,
-      currentSort: sort,
-      totalDocuments,
-    });
+      res.render("error.ejs", { err });
   }
 });
 
-router.get("/delete", async (req, res) => {
-  const page = +req.query.page || 1;
-  const sort = req.query.sort || "asc";
-
-  try {
-    const id = req.query.id;
-    await Task.deleteOne({ _id: id });
-    res.redirect(301, "/?page=" + page + "&sort=" + sort);
-  } catch (err) {
-    res.render("error.ejs", { err });
-  }
-});
-
-// Temporary post route for form delete combined with id and pagination. To be removed once we have a link and span that resembles a checkbox.
-router.post("/delete", async (req, res) => {
-  const page = +req.query.page || 1;
-  const sort = req.query.sort || "asc";
-
-  try {
-    const id = req.query.id;
-    await Task.deleteOne({ _id: id });
-    res.redirect(301, "/?page=" + page + "&sort=" + sort);
-  } catch (err) {
-    res.render("error.ejs", { err });
-  }
-});
+router.get("/delete", deleteMiddleware);
+router.post("/delete", deleteMiddleware);
 
 router.get("*", (req, res) => {
   res.render("error.ejs", {
